6d42716c7754e31f6a5736fd45f80d3f171e8050
iam-Akash-Mohan <akash.mohan@growfin.ai>
2025-02-09 12:55:16 +0530

    first commit

---
+++ src/test/java/com/akash/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
